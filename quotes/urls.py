b92e1c6778cdc90d24437acf1132187df7e7e92a
amir3483 <ashoordehi66@yahoo.com>
2022-11-16 01:44:54 +0330

    commit 1

---
+++ quotes/urls.py
@@ -0,0 +1,13 @@
+"""
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+"""
+
+from . import views
+from django.urls import path , include
+# from django.conf.urls import url
+from django.urls import re_path as url
+urlpatterns = [
+    
+]
