54d1533cd4d453ef1db2d97f6fdc74d5f43feac9
amir3483 <ashoordehi66@yahoo.com>
2022-11-20 03:26:54 +0330

    commit 3

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request): 
-    return render(request , 'home.html' , {})
+    import requests
+    import json
+    # pk_5962d03a35b9408cb94adba159d26f73
+    
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_5962d03a35b9408cb94adba159d26f73")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error...."
+            
+    
+    return render(request , 'home.html' , {'api': api})
 
 def about(request): 
     return render(request , 'about.html' , {})
