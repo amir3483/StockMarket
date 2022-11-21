e5b90bf85ba73c636f3b39a6d5927c645348f532
amir3483 <ashoordehi66@yahoo.com>
2022-11-21 05:05:28 +0330

    commit 4

--- quotes/views.py
+++ quotes/views.py
@@ -3,20 +3,27 @@ from django.shortcuts import render
 def home(request): 
     import requests
     import json
-    # pk_5962d03a35b9408cb94adba159d26f73
     
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_5962d03a35b9408cb94adba159d26f73")
+    if request.method == 'POST' :
+       ticker = request.POST['ticker']
+       api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_5962d03a35b9408cb94adba159d26f73")
     
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error...."
-            
-    
-    return render(request , 'home.html' , {'api': api})
+       try:
+           api = json.loads(api_request.content)
+       except Exception as e:
+           api = "Error...."
+       return render(request , 'home.html' , {'api' : api})
+   
+    else:
+        return render(request , 'home.html' , {'ticker': "Enter a ticker"})
+ 
 
 def about(request): 
-    return render(request , 'about.html' , {})
+    import requests
+    import json
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_5962d03a35b9408cb94adba159d26f73")
+    api = json.loads(api_request.content)
+    return render(request , 'about.html' , {'api' : api})
 
 def base(request):
     return render(request , 'base.html' , {})
